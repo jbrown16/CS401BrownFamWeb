33e20ab5b68c9a8fc290dbec8d9864f569d0423f
Justin Brown <justin.brown15@outlook.com>
2018-04-12 00:45:28 -0600

    jQuery changes to fam-member pages

--- activities-page.php
+++ activities-page.php
@@ -8,7 +8,8 @@
     <head>
         <title>Family Activities</title>
         <link href="/images/favicon.ico" rel="shortcut icon">
-		<link rel="stylesheet" href="/styles/activities-style.css">
+        <link rel="stylesheet" href="/styles/activities-style.css">
+        <script type="text/javascript" src="/jquery-src/jquery-3.3.1.min.js"></script>
     </head>
     <body class="members">
         <div id="container">
@@ -20,8 +21,16 @@
                     <a href="logout.php" id="log-out-btn">Log out</a>
                 </div>
             </div>
-            <div class="nav-bar">
-                <ul>
+            <div class="nav-bar myMenu">
+                <ul class="dropDownMenu">
+                    <li class="fam-activity">
+                        <a href="#">MAIN</a>
+                            <ul>
+                                <li><a href="/members-page.php">Members</a></li>
+                                <li><a href="/activities-page.php">Activities</a></li>
+                                <li><a href="/vacations-page.php">Vacations</a></li>
+                            </ul>
+                    </li>
                     <li class="fam-activity">
                         <a href="/activities-main/summer.php">Summer</a>
                     </li>
@@ -45,5 +54,12 @@
                 thebrownfamily.com | Activities
             </div>
         </div>
+
+        <script type="text/javascript">
+            $('.myMenu ul li').hover(function() {
+                $(this).children('ul').stop(true, false, true).slideToggle(300);
+            });
+        </script>
+        
     </body>
 </html>
--- vacations-page.php
+++ vacations-page.php
@@ -8,7 +8,8 @@
     <head>
         <title>Family Vacations</title>
         <link href="/images/favicon.ico" rel="shortcut icon">
-		<link rel="stylesheet" href="/styles/vacations-style.css">
+        <link rel="stylesheet" href="/styles/vacations-style.css">
+        <script type="text/javascript" src="/jquery-src/jquery-3.3.1.min.js"></script>
     </head>
     <body class="members">
         <div id="container">
@@ -20,8 +21,16 @@
                     <a href="/logout.php" id="log-out-btn">Log out</a>
                 </div>
             </div>
-            <div class="nav-bar">
-                <ul>
+            <div class="nav-bar myMenu">
+                <ul class="dropDownMenu">
+                    <li class="fam-vacation">
+                        <a href="#">MAIN</a>
+                            <ul>
+                                <li><a href="/members-page.php">Members</a></li>
+                                <li><a href="/activities-page.php">Activities</a></li>
+                                <li><a href="/vacations-page.php">Vacations</a></li>
+                            </ul>
+                    </li>
                     <li class="fam-vacation">
                         <a href="#">California Trip</a>
                     </li>
@@ -45,5 +54,12 @@
                 thebrownfamily.com | Vacations
             </div>
         </div>
+
+        <script type="text/javascript">
+            $('.myMenu ul li').hover(function() {
+                $(this).children('ul').stop(true, false, true).slideToggle(300);
+            });
+        </script>
+
     </body>
 </html>
